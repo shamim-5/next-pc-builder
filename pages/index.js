0c279d0fb7b9df1e203a5640efae97f42f61a130
shamim-5 <shamim.somadder5@gmail.com>
2023-07-30 03:27:04 +0600

    setup antd, tailwindcss, daisyui and some global css

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
-export default function Home() {
-  return (
-    <>
-      <h1>Project cleaned</h1>
-    </>
-  );
-}
+import { Button } from "antd";
+
+const HomePage = () => (
+  <>
+    <Button className="text-red-900" type="primary">
+      Test Style
+    </Button>
+
+    <h1 className="text-red-900 ">Hello H1</h1>
+    <h4 className="text-3xl font-semibold">Hello H4</h4>
+  </>
+);
+
+export default HomePage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    // "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+    extend: {},
   },
-  plugins: [],
-}
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#a991f7",
+          secondary: "#f6d860",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+
+  // antd configuration
+  corePlugins: {
+    preflight: false,
+  },
+  important: true,
+};
